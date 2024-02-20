e9040432248e953087a985b8a66e3891fc231c6a
Bram Fokke <bram.fokke@ambacht.it>
2024-02-20 15:30:25 +0100

    Made state a bit easier, separate events are not really needed.

--- BlazorRxMouse.Client/MouseState.cs
+++ BlazorRxMouse.Client/MouseState.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using System.Reactive.Subjects;
+using System.Reactive;
 
 namespace BlazorRxMouse.Client
 {
@@ -15,10 +17,49 @@ namespace BlazorRxMouse.Client
 		}
 		private MousePosition _position;
 
-		public MouseButtonState Left { get; } = new();
-		public MouseButtonState Right { get; } = new();
-		public MouseButtonState Middle { get; } = new();
+		/// <summary>
+		/// Is the left button currently pressed?
+		/// </summary>
+		public bool LeftButtonPressed
+		{
+			get => _leftButtonPressed;
+			set => SetField(ref _leftButtonPressed, value);
+		}
 
+		private bool _leftButtonPressed;
+
+		/// <summary>
+		/// Is the middle button currently pressed?
+		/// </summary>
+		public bool MiddleButtonPressed
+		{
+			get => _middleButtonPressed;
+			set => SetField(ref _middleButtonPressed, value);
+		}
+
+		private bool _middleButtonPressed;
+
+
+		/// <summary>
+		/// Is the right button currently pressed?
+		/// </summary>
+		public bool RightButtonPressed
+		{
+			get => _rightButtonPressed;
+			set => SetField(ref _rightButtonPressed, value);
+		}
+
+		private bool _rightButtonPressed;
+
+		/// <summary>
+		/// Is the cursor currently hovering over the layer intercepting the events
+		/// </summary>
+		public bool Over
+		{
+			get => _over;
+			set => SetField(ref _over, value);
+		}
+		private bool _over;
 
 	}
 }
